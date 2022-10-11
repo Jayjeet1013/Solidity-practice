fae44785f872fb529a7e5dbe76ac23e6fdb38c19
Jayjeet kumar <80195355+Jayjeet1013@users.noreply.github.com>
2022-10-11 10:56:17 +0530

    Create day1.sol

---
+++ day1.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.15;
+
+contract assignment{
+
+   uint public count = 8;
+   function increment(uint count1) public  returns(uint) {
+      return count1 = count++;
+   }
+  function decrement(uint count2) public returns(uint){
+      return count2=count--;
+  }
+
+}
+
