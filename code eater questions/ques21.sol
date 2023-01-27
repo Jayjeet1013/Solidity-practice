4119d3d08577401fc13a5f0a24034cd82207cf92
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-28 01:59:36 +0530

    done ques21

--- code eater questions/ques21.sol
+++ code eater questions/ques21.sol
@@ -2,4 +2,12 @@
 
 pragma solidity^0.8.17;
 
-contract 
+contract reversearray{
+    function array(int[5] memory arr) public pure returns(int[5] memory){
+        int[5] memory reverseArr;
+        for(uint i=0;i<arr.length;i++){
+            reverseArr[arr.length-(i+1)]=arr[i];
+        }
+          return reverseArr;
+    }
+}
