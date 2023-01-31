90e95024d8b8e269610829729c6b1b4139f6577e
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-31 18:13:06 +0530

    ques15 update

--- code eater questions/ques25.sol
+++ code eater questions/ques25.sol
@@ -2,4 +2,24 @@
 
 pragma solidity ^0.8.17;
 
-contract 
+
+contract studentDetails{
+    struct student{
+        string name;
+        uint roll;
+        uint age;
+    }
+
+    student[10] details;
+    uint index;
+    function insert(string calldata _name, uint _roll, uint _age) public{
+        details[index].name=_name;
+        details[index].roll=_roll;
+        details[index].age=_age;
+        index++;
+    }
+
+    function returndata() public view returns(student[10] memory){
+        return details;
+    }
+}
