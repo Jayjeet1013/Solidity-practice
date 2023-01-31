affb474e442634188ff0ecfd31a716e8182bf732
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-31 18:08:27 +0530

    ques24 update

---
+++ code eater questions/ques24.sol
@@ -0,0 +1,25 @@
+//student filling form
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract studentDetails{
+    struct student{
+        string name;
+        uint roll;
+        uint age;
+    }
+
+    student details;
+
+    function insert(string calldata _name, uint _roll, uint _age) public{
+        details.name=_name;
+        details.roll=_roll;
+        details.age=_age;
+    }
+
+    function returndata() public view returns(student memory){
+        return details;
+    }
+}
