4d8f4d7a8b0f202c8108274d5091e2e8331dafb1
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-05 23:03:43 +0530

    update

---
+++ Oops in solidity/interface.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIt
+
+pragma solidity^0.8.17;
+
+interface inter{
+    function add(uint n1, uint n2) external;
+    function substract(uint n3,uint n4) external;
+}
+
+contract page is inter{
+    function add(uint n1,uint n2) external  override{
+      
+    }
+        function substract(uint n3,uint n4) external override{
+
+        }
+}
