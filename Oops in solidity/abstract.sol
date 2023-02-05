a189435ff8adf240ea4222e539135d5dc83525e0
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-05 22:50:46 +0530

    update

--- Oops in solidity/abstract.sol
+++ Oops in solidity/abstract.sol
@@ -3,6 +3,26 @@
 
 pragma solidity^0.8.17;
 
-contract abstdract{
+abstract contract abstracts{
+     uint height;
+     uint breadth;
+     uint length;
 
+     function dimension(uint _height, uint _breadth,uint _length) public {
+        length=_length;
+        breadth=_breadth;
+        height=_height;
+     }
+
+     function getdimen() public view returns(uint ,uint ,uint){
+        return (height,breadth,length);
+     }
+
+     function add(uint a,uint b) public virtual;
+}
+
+contract dimen  is abstracts{
+function add(uint a,uint b) public pure override{
+    a=b;
+}
 }
