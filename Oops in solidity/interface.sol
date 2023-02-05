e77579183a9a7f983e0842d11b7c99583e2d18e6
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-06 00:01:54 +0530

    update

--- Oops in solidity/interface.sol
+++ Oops in solidity/interface.sol
@@ -5,6 +5,7 @@ pragma solidity^0.8.17;
 interface inter{
     function add(uint n1, uint n2) external;
     function substract(uint n3,uint n4) external;
+    function multiplication(uint a, uint b) external;
 }
 
 contract page is inter{
@@ -14,4 +15,8 @@ contract page is inter{
         function substract(uint n3,uint n4) external override{
 
         }
+
+     function multiplication(uint a,uint b) external override{
+
+     }
 }
